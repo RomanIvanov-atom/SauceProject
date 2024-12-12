872a5555f5e88a119f850cb76401969949351d19
RomanR <ryabychr@eastmining.ru>
2024-12-12 19:27:04 +0300

    added page

--- src/test/java/products/Credentials.java
+++
@@ -1,7 +0,0 @@
-package products;
-
-public class Credentials {
-    public static final String VALID_NAME = "testName";
-    public static final String VALID_LAST_NAME = "testName";
-    public static final String VALID_ZIP_CODE = "testZipCode";
-}
